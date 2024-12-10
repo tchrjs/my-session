1c9f141d556292e5e533a1112ab979b789d1d425
Christian Toribio <chris09toribio@gmail.com>
2024-12-10 07:36:38 -0800

    intial setup for pwa

--- app/components/bottombar.js
+++ app/components/bottombar.js
@@ -12,35 +12,35 @@ export default function BottomBar() {
   const currentPath = usePathname(); // Get the current route
 
   return (
-    <footer className="fixed bottom-0 w-full h-16 flex justify-center items-center p-4">
-      <div className="w-full h-full flex justify-around items-center">
+    <footer className="fixed bottom-0 w-full h-20 flex justify-center items-center p-4">
+      <div className="w-full h-full flex justify-around items-start">
         <Link href={"/"}>
           <div
-            className={`flex flex-col justify-center items-center gap-2 ${
+            className={`flex flex-col justify-center items-center ${
               currentPath === "/" ? "text-green-400" : "text-gray-400"
             }`}
           >
-            <BsFillClipboard2DataFill className="scale-125" />
+            <BsFillClipboard2DataFill />
             <div className="text-xs">summary</div>
           </div>
         </Link>
         <Link href={"/routines"}>
           <div
-            className={`flex flex-col justify-center items-center gap-2 ${
+            className={`flex flex-col justify-center items-center ${
               currentPath === "/routines" ? "text-green-400" : "text-gray-400"
             }`}
           >
-            <FaWalking className="scale-125" />
+            <FaWalking />
             <div className="text-xs">routines</div>
           </div>
         </Link>
         <Link href={"/sessions"}>
           <div
-            className={`flex flex-col justify-center items-center gap-2 ${
+            className={`flex flex-col justify-center items-center ${
               currentPath === "/sessions" ? "text-green-400" : "text-gray-400"
             }`}
           >
-            <RiCalendarTodoFill className="scale-125" />
+            <RiCalendarTodoFill />
             <div className="text-xs">sessions</div>
           </div>
         </Link>
---
+++ app/manifest.js
@@ -0,0 +1,23 @@
+export default function manifest() {
+  return {
+    name: "My Session",
+    short_name: "My Session",
+    description: "h",
+    start_url: "/routines",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/favicon/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/favicon/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
