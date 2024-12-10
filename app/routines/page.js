0e298966ffa5bb7535282949ab6d4fb8875bf74d
Christian Toribio <chris09toribio@gmail.com>
2024-12-09 16:52:03 -0800

    removed popover hover effect

--- app/routines/page.js
+++ app/routines/page.js
@@ -24,7 +24,6 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
-import { Button } from "@/components/ui/button";
 import { FaDumbbell, FaWalking } from "react-icons/fa";
 
 export default function Page() {
@@ -48,14 +47,14 @@ export default function Page() {
             </PopoverTrigger>
             <PopoverContent className="w-52 flex flex-col bg-neutral-800 -translate-x-2 translate-y-4">
               <div
-                className={`w-full flex p-2 items-center opacity-50 hover:opacity-100 border-b-[1px] border-neutral-500`}
+                className={`w-full flex p-2 items-center border-b-[1px] border-neutral-500`}
               >
                 <div className="w-5/6">Create a routine</div>
                 <div className="w-1/6 flex justify-end">
                   <FaWalking />
                 </div>
               </div>
-              <div className="flex p-2 items-center opacity-50 hover:opacity-100">
+              <div className="flex p-2 items-center">
                 <div className="w-5/6">Create a workout</div>
                 <div className="w-1/6 flex justify-end">
                   <FaDumbbell />
