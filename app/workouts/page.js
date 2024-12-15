c5930bca4e0192b00ca093f63b77e7b75892bf2c
Christian Toribio <chris09toribio@gmail.com>
2024-12-14 20:13:50 -0800

    added dropdown menu for workouts

--- app/workouts/page.js
+++ app/workouts/page.js
@@ -2,10 +2,9 @@
 
 import { useState } from "react";
 import { LiaSearchSolid } from "react-icons/lia";
+
 import WorkoutDrawer from "@/components/workoutdrawer/workoutdrawer";
-import { FaEllipsisH } from "react-icons/fa";
-import { FiChevronDown } from "react-icons/fi";
-import { Separator } from "@/components/ui/separator";
+import WorkoutItem from "@/components/workoutitem/workoutitem";
 
 export default function Page() {
   const [workouts, setWorkouts] = useState([]);
@@ -40,21 +39,7 @@ export default function Page() {
         {/* workouts */}
         <div className="w-full h-full flex flex-col items-center gap-2">
           {workouts.map((workout, i) => (
-            <div key={i} className="w-full bg-neutral-800">
-              <div className="w-full flex items-center px-4 py-2">
-                <div className="w-full flex justify-start">{workout.name}</div>
-                <div className="flex gap-4">
-                  <FiChevronDown className="scale-125" />
-                  <FaEllipsisH />
-                </div>
-              </div>
-              <Separator className="border-b-[1px] border-neutral-700" />
-              <div className="w-full flex px-4 p-2 text-neutral-400 gap-6">
-                <div className="text-xs ">
-                  {workout.type + " - " + workout.sets.length + " sets"}
-                </div>
-              </div>
-            </div>
+            <WorkoutItem key={i} workout={workout} />
           ))}
         </div>
       </main>
---
+++ components/workoutitem/workoutitem.js
@@ -0,0 +1,71 @@
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+
+import { FaEllipsisH } from "react-icons/fa";
+import { FiChevronDown, FiEdit2, FiTrash2 } from "react-icons/fi";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+
+export default function WorkoutItem({ workout }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <div className="w-full">
+      <div className="w-full flex items-center px-4 py-2 bg-neutral-800">
+        <div className="w-full flex justify-start">{workout.name}</div>
+        <div className="flex gap-4">
+          <button type="button" onClick={() => setIsExpanded(!isExpanded)}>
+            <FiChevronDown
+              className={`${
+                isExpanded ? "-rotate-180" : ""
+              } transition-transform scale-125`}
+            />
+          </button>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <FaEllipsisH />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="bg-neutral-900">
+              <DropdownMenuItem>
+                <FiEdit2 />
+                Edit
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <FiTrash2 />
+                Delete
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+      <Separator className="border-b-[1px] border-neutral-700" />
+      <div
+        className={`w-full flex px-4 p-2 bg-neutral-800 text-neutral-400 text-sm ${
+          isExpanded ? "hidden" : ""
+        }`}
+      >
+        {`${workout.type} ${
+          workout.sets && workout.sets.length !== 0
+            ? "- " + workout.sets.length + " sets"
+            : ""
+        }`}
+      </div>
+      <div
+        className={`w-full flex px-4 p-2 bg-neutral-800 text-neutral-300 text-sm ${
+          isExpanded ? "" : "hidden"
+        }`}
+      >
+        set
+      </div>
+    </div>
+  );
+}
