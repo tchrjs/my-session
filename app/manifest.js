cd53e083791ce0cb919c1c77e53eb5e428eef9fc
Christian Toribio <chris09toribio@gmail.com>
2025-01-07 16:56:26 -0800

    use tailwind bg colors for background and theme

--- app/manifest.js
+++ app/manifest.js
@@ -1,10 +1,17 @@
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "@/tailwind.config";
+
+const fullConfig = resolveConfig(tailwindConfig);
+
 export default function manifest() {
+  const bg_color = fullConfig.theme.colors.background;
+
   return {
     name: "My Session",
     short_name: "My Session",
     display: "standalone",
-    background_color: "bg-background",
-    theme_color: "bg-background",
+    background_color: bg_color,
+    theme_color: bg_color,
     icons: [
       {
         src: "/favicon/web-app-manifest-192x192.png",
