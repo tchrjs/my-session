39d6772632c60b6bc92385795afcb772f2190d32
Christian Toribio <chris09toribio@gmail.com>
2025-01-07 17:19:06 -0800

    made manifest colors hard coded

--- app/manifest.js
+++ app/manifest.js
@@ -1,17 +1,10 @@
-import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "@/tailwind.config";
-
-const fullConfig = resolveConfig(tailwindConfig);
-
 export default function manifest() {
-  const bg_color = fullConfig.theme.colors.background;
-
   return {
     name: "My Session",
     short_name: "My Session",
     display: "standalone",
-    background_color: bg_color,
-    theme_color: bg_color,
+    background_color: "#0a0a0a",
+    theme_color: "#0a0a0a",
     icons: [
       {
         src: "/favicon/web-app-manifest-192x192.png",
